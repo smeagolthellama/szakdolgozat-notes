c8e03be35f01af2388594c114087f613aec946af
kocka_collector <babiak@disroot.org>
2022-12-21 17:47:28 +0200

    start on rewrite in ada

---
+++ ada/src/build.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO;
+with GNAT.Sockets;
+with Network_Tree;
+
+procedure Build is
+
+begin
+   --  Insert code here.
+   null;
+end Build;
---
+++ ada/src/network_tree.ads
@@ -0,0 +1,7 @@
+with GNAT.Sockets;
+
+package Network_Tree is
+   package Sock renames GNAT.Sockets;
+   port: Sock.Port_Type;
+
+end Network_Tree;
