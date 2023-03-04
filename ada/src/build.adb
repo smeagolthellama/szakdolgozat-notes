14b74718d2426769763348b4343f12bf876b2aac
kocka_collector <babiak@disroot.org>
2023-03-04 12:44:22 +0200

    comment and harden input testing

--- ada/src/build.adb
+++ ada/src/build.adb
@@ -17,15 +17,18 @@ begin
         (Standard_Error,
          "Usage: " & Ada.Command_Line.Command_Name &
          " [options] host[:port] [...]");
+      Put_Line
+        (Standard_Error,
+         "Starting in server-only mode. Listening on port" & Port'Image &
+         " on family " & Family'Image & " (Inet: IPv4)");
    end if;
+   -- parse simple command line options
    loop
       case GNAT.Command_Line.Getopt ("h p: 6 -help -port=") is
          when 'h' =>
             Print_Help := True;
          when 'p' =>
-            Port :=
-              GNAT.Sockets.Port_Type
-                (Integer'Value (GNAT.Command_Line.Parameter));
+            Port := GNAT.Sockets.Port_Type'Value (GNAT.Command_Line.Parameter);
          when '6' =>
             Family := GNAT.Sockets.Family_Inet6;
          when '-' =>
@@ -33,90 +36,107 @@ begin
                Print_Help := True;
             elsif GNAT.Command_Line.Full_Switch = "--port" then
                Port :=
-                 GNAT.Sockets.Port_Type
-                   (Integer'Value (GNAT.Command_Line.Parameter));
+                 GNAT.Sockets.Port_Type'Value (GNAT.Command_Line.Parameter);
             end if;
          when others =>
             exit;
       end case;
    end loop;
-   if Print_Help then
-      null;
-   else
 
+   if Print_Help then
+      null; -- TODO : print help mesage
+   else
+      -- Parse servers to try listening to, in the form of ip addresses with ports.
+      -- accepted formats:
+      -- ipv4, ipv6, [ipv4], [ipv6], ipv4:port, [ipv6]:port
       declare
          package my_tree is new Network_Tree (family => Family, port => Port);
          End_Of_Arguments : Boolean := False;
-         Try_Again    : exception;
          Format_Error : exception;
       begin
          while not End_Of_Arguments loop
             declare
+               -- get next command line argument
                Argument : constant String :=
                  GNAT.Command_Line.Get_Argument
                    (End_Of_Arguments => End_Of_Arguments);
                use Ada.Strings.Fixed;
             begin
                pragma Debug
-                 (Put_Line (Standard_Error, "parsing '" & Argument & "'."));
-               if Argument'Length = 0 then
-                  raise Try_Again; -- continue the loop
-               end if;
-               if GNAT.Sockets.Is_IPv4_Address (Argument)
-                 or else GNAT.Sockets.Is_IPv6_Address (Argument)
-               then
-                  my_tree.Client (GNAT.Sockets.Inet_Addr (Argument), Port);
-               elsif Argument (1) = '[' then
-                  declare
-                     Close_Bracket : constant Natural := Index (Argument, "]");
-                  begin
-                     if GNAT.Sockets.Is_IPv6_Address
-                         (Name => Argument (2 .. Close_Bracket - 1))
-                       or else GNAT.Sockets.Is_IPv4_Address
-                         (Name => Argument (2 .. Close_Bracket - 1))
-                     then
-                        if Close_Bracket = Argument'Last then
-                           my_tree.Client
-                             (GNAT.Sockets.Inet_Addr
-                                (Argument (2 .. Close_Bracket - 1)),
-                              Port);
-                        elsif Argument (Close_Bracket + 1) = ':' then
-                           my_tree.Client
-                             (GNAT.Sockets.Inet_Addr
-                                (Argument (2 .. Close_Bracket - 1)),
-                              GNAT.Sockets.Port_Type'Value
-                                (Argument
-                                   (Close_Bracket + 2 .. Argument'Last)));
-                           -- If not numeric, will raise an exception.
+                 (Put_Line (Standard_Error, "Parsing '" & Argument & "'."));
+               if Argument'Length /= 0 then
+                  if GNAT.Sockets.Is_IPv4_Address (Argument)
+                    or else GNAT.Sockets.Is_IPv6_Address (Argument)
+                  then
+                     -- note that Is_IPv4 accepts some addresses that Inet_Addr does not.
+                     -- In these cases, Inet_Addr raises a Socket_Error
+                     my_tree.Client (GNAT.Sockets.Inet_Addr (Argument), Port);
+                  elsif Argument (1) = '[' then
+                     declare
+                        -- If there is not ']' in Argument, Close_Bracket will be 0
+                        -- in which case, I will soon try to access
+                        -- Argument (2 .. -1) which will raise a Constraint_Error.
+                        Close_Bracket : constant Natural :=
+                          Index (Argument, "]");
+                     begin
+                        if GNAT.Sockets.Is_IPv6_Address
+                            (Name => Argument (2 .. Close_Bracket - 1))
+                          or else GNAT.Sockets.Is_IPv4_Address
+                            (Name => Argument (2 .. Close_Bracket - 1))
+                        then
+                           if Close_Bracket = Argument'Last then
+                              -- looks like it's an [ipv6] or [ipv4]. If not, Socket_Error.
+                              my_tree.Client
+                                (GNAT.Sockets.Inet_Addr
+                                   (Argument (2 .. Close_Bracket - 1)),
+                                 Port);
+                           elsif Argument (Close_Bracket + 1) = ':' then
+                              -- We know it's [ipv*]:*.
+                              -- We try to parse the port, and let Ada do it's thing.
+                              my_tree.Client
+                                (GNAT.Sockets.Inet_Addr
+                                   (Argument (2 .. Close_Bracket - 1)),
+                                 GNAT.Sockets.Port_Type'Value
+                                   (Argument
+                                      (Close_Bracket + 2 .. Argument'Last)));
+                              -- If the port is not numeric, 'Value will raise an exception.
+                           else
+                              -- It's [ipv*]\[^:\]* Not a clue what to do with it.
+                              raise Format_Error
+                                with "The closing bracket is not the last character, or followed by a colon.";
+                           end if;
                         else
                            raise Format_Error
-                             with "The closing bracket is not the last character, or followed by a colon.";
+                             with "The bracketed segment is not an IP address.";
                         end if;
+                     end;
+                  else -- should be an ipv4 address with a port
+                     if Ada.Strings.Fixed.Count (Argument, ":") > 1
+                     then -- too many colons
+                        raise Format_Error
+                          with "'" &
+                          Argument (Index (Argument, ":") .. Argument'Last) &
+                          "' is not a Natural number (for IPv6, the expected format is along the lines of [::1]:1234)";
+                     elsif Ada.Strings.Fixed.Count (Argument, ":") /= 1
+                     then --not enough colons
+                        raise Format_Error
+                          with "Argument '" & Argument &
+                          "' was not recognised as an IP address (with an optional port).";
                      end if;
-                  end;
-               else -- should be an ipv4 address with a port
-                  if Ada.Strings.Fixed.Count (Argument, ":") > 1 then
-                     raise Format_Error
-                       with "'" &
-                       Argument (Index (Argument, ":") .. Argument'Last) &
-                       "' is not a Natural number (for IPv6, the expected format is along the lines of [::1]:1234)";
-                  elsif Ada.Strings.Fixed.Count (Argument, ":") /= 1 then
-                     raise Format_Error
-                       with "Argument '" & Argument &
-                       "' was not recognised as an IP address (with an optional port).";
+                     declare
+                        Colon : constant Natural := Index (Argument, ":");
+                     begin
+                        if GNAT.Sockets.Is_IPv4_Address
+                            (Argument (1 .. Colon - 1))
+                        then
+                           my_tree.Client
+                             (GNAT.Sockets.Inet_Addr
+                                (Argument (1 .. Colon - 1)),
+                              GNAT.Sockets.Port_Type'Value
+                                (Argument (Colon + 1 .. Argument'Last)));
+                        end if;
+                     end;
                   end if;
-                  declare
-                     Colon : constant Natural := Index (Argument, ":");
-                  begin
-                     if GNAT.Sockets.Is_IPv4_Address
-                         (Argument (1 .. Colon - 1))
-                     then
-                        my_tree.Client
-                          (GNAT.Sockets.Inet_Addr (Argument (1 .. Colon - 1)),
-                           GNAT.Sockets.Port_Type'Value
-                             (Argument (Colon + 1 .. Argument'Last)));
-                     end if;
-                  end;
                end if;
             exception
                when E : GNAT.Sockets.Socket_Error =>
@@ -128,7 +148,8 @@ begin
                when E : Constraint_Error =>
                   Put_Line
                     (Standard_Error,
-                     "Error parsing argument '" & Argument & "':");
+                     "Error parsing argument '" & Argument &
+                     "' (no closing bracket?):");
                   Put_Line
                     (Standard_Error, Ada.Exceptions.Exception_Message (E));
                when E : Format_Error =>
@@ -137,8 +158,6 @@ begin
                      "Error parsing argument '" & Argument & "':");
                   Put_Line
                     (Standard_Error, Ada.Exceptions.Exception_Message (E));
-               when Try_Again =>
-                  null; -- Used for a continue equivalent
                when others =>
                   Put_Line
                     (Standard_Error,
