1f1805bd773c736138b03d3c3a7442a2fe5373ae
kocka_collector <babiak@disroot.org>
2023-04-08 14:28:53 +0300

    format files

--- ada/src/network_tree.ads
+++ ada/src/network_tree.ads
@@ -2,15 +2,16 @@ with GNAT.Sockets;
 with Ada.Streams;
 
 generic
-   family : in GNAT.Sockets.Family_Inet_4_6 := GNAT.Sockets.Family_Inet;
-   port : in GNAT.Sockets.Port_Type := 10_573;
-   maxMessageLength : in Ada.Streams.Stream_Element_Offset := 2_048;
+    family : in GNAT.Sockets.Family_Inet_4_6 := GNAT.Sockets.Family_Inet;
+    port : in GNAT.Sockets.Port_Type := 10_573;
+    maxMessageLength : in Ada.Streams.Stream_Element_Offset := 2_048;
 package Network_Tree is
-   pragma Assert (Ada.Streams.Stream_Element'Size = 8);
+    pragma Assert (Ada.Streams.Stream_Element'Size = 8);
 
-   package Sock renames GNAT.Sockets;
+    package Sock renames GNAT.Sockets;
 
-   task Server;
+    task Server;
 
-   procedure Connect_To_Server (addr : Sock.Inet_Addr_Type; port : Sock.Port_Type);
+    procedure Connect_To_Server
+       (addr : Sock.Inet_Addr_Type; port : Sock.Port_Type);
 end Network_Tree;
